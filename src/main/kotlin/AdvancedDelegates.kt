03838c1fc9aace939b4f99f3a042a17dda2d114f
Sulaiman Oboody <lsafer@cufy.org>
2023-05-20 18:42:17 +0300

    feat: Advanced delegates

---
+++ src/main/kotlin/AdvancedDelegates.kt
@@ -0,0 +1,114 @@
+/*
+ *	Copyright 2023 cufy.org
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *	    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package net.lsafer.enver
+
+import kotlin.properties.ReadOnlyProperty
+
+//
+
+/**
+ * A wrapper over [optional] only accepting
+ * contentful strings. (aka. non blank strings)
+ *
+ * @param name the variable's name.
+ * @since 1.0.0
+ */
+fun Enver.contentful(name: String): ReadOnlyProperty<Any?, String?> {
+    return optional(name) { it?.takeIf { it.isNotBlank() } }
+}
+
+/**
+ * A wrapper over [optional] only accepting
+ * contentful strings. (aka. non blank strings)
+ *
+ * @param name the variable's name.
+ * @param block a function for transforming the value to type of [T].
+ *               Invoked once on every change of the variable's name.
+ * @since 1.0.0
+ */
+fun <T> Enver.contentful(name: String, block: (String?) -> T): ReadOnlyProperty<Any?, T> {
+    return optional(name) { block(it?.takeIf { it.isNotBlank() }) }
+}
+
+//
+
+/**
+ * A wrapper over [optional] only accepting
+ * contentful strings. (aka. non blank strings)
+ *
+ * @param name the variable's name.
+ * @param default the default value.
+ * @since 1.0.0
+ */
+fun Enver.contentful(name: String, default: String): ReadOnlyProperty<Any?, String?> {
+    return optional(name) { it?.takeIf { it.isNotBlank() } ?: default }
+}
+
+/**
+ * A wrapper over [optional] only accepting
+ * contentful strings. (aka. non blank strings)
+ *
+ * @param name the variable's name.
+ * @param default the default value.
+ * @param block a function for transforming the value to type of [T].
+ *               Invoked once on every change of the variable's name.
+ * @since 1.0.0
+ */
+fun <T> Enver.contentful(name: String, default: String, block: (String) -> T): ReadOnlyProperty<Any?, T> {
+    return optional(name) { block(it?.takeIf { it.isNotBlank() } ?: default) }
+}
+
+//
+
+fun Enver.int(name: String): ReadOnlyProperty<Any?, Int?> {
+    return optional(name) { it?.toIntOrNull() }
+}
+
+fun <T> Enver.int(name: String, block: (Int?) -> T): ReadOnlyProperty<Any?, T> {
+    return optional(name) { block(it?.toIntOrNull()) }
+}
+
+//
+
+fun Enver.int(name: String, default: Int): ReadOnlyProperty<Any?, Int> {
+    return optional(name) { it?.toIntOrNull() ?: default }
+}
+
+fun <T> Enver.int(name: String, default: Int, block: (Int) -> T): ReadOnlyProperty<Any?, T> {
+    return optional(name) { block(it?.toIntOrNull() ?: default) }
+}
+
+//
+
+fun Enver.long(name: String): ReadOnlyProperty<Any?, Long?> {
+    return optional(name) { it?.toLongOrNull() }
+}
+
+fun <T> Enver.long(name: String, block: (Long?) -> T): ReadOnlyProperty<Any?, T> {
+    return optional(name) { block(it?.toLongOrNull()) }
+}
+
+//
+
+fun Enver.long(name: String, default: Long): ReadOnlyProperty<Any?, Long> {
+    return optional(name) { it?.toLongOrNull() ?: default }
+}
+
+fun <T> Enver.long(name: String, default: Long, block: (Long) -> T): ReadOnlyProperty<Any?, T> {
+    return optional(name) { block(it?.toLongOrNull() ?: default) }
+}
+
+//
